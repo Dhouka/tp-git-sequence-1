23f809e6f70dc66e80b01e3cf5074d45f6f45e81
dhouka <dhoukakooli12@gmail.com>
2025-01-31 18:57:04 +0100

    Modification de la fonction bienvenue

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,27 +1,9 @@
 
 // Affiche un message de bienvenue
 #include <iostream>
-int main()
-{
-std::cout << "Bienvenue le monde !" << std::endl;
-return 0;
-}
-// Affiche un message de bienvenue
-#include <iostream>
-int main() {
-    std::cout << "Bienvenue " << std::endl;
-    return 0;
-}
-#include "fonction-bienvenue.h"
 
 int main() {
-    afficherBienvenue();
+    std::cout << "Bienvenue le monde !" << std::endl;
     return 0;
 }
-// Affiche un message de bienvenue
-#include "fonction-bienvenue.h"
-int main()
-{
-afficherBienvenue();
-return 0;
-}
+
